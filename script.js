b412288adb9e1fdc1cc7fc16c256085c3cadfef9
Jay <cleve703@gmail.com>
2020-11-29 20:48:57 -0500

    add x and o to board, check for win

--- script.js
+++ script.js
@@ -1,46 +1,112 @@
-const gameBoard = (() => {
-  function Space(grid, marker) {
-    this.grid = grid
-    this.marker = marker
+var gameBoard = (function() {
+  class Space {
+    constructor(grid, marker) {
+      this.grid = grid;
+      this.marker = marker;
+    }
+    assignMarker(marker) {
+      this.marker = marker;
+    }
   }
 
-  Space.prototype.assignMarker = function(marker) {
-    this.marker = marker;
-  }
-
-  var a1 = new Space("a1", "X");
-  var a2 = new Space("a2", "O");
+  
+  var a1 = new Space("a1", "");
+  var a2 = new Space("a2", "");
   var a3 = new Space("a3", "");
   var b1 = new Space("b1", "");
   var b2 = new Space("b2", "");
-  var b3 = new Space("b3", "X");
-  var c1 = new Space("c1", "O");
-  var c2 = new Space("c2", "X");
+  var b3 = new Space("b3", "");
+  var c1 = new Space("c1", "");
+  var c2 = new Space("c2", "");
   var c3 = new Space("c3", "");
 
-  var spaces = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
+  var spaces = [a1, a2, a3, b1, b2, b3, c1, c2, c3];
+  var step = 0;
 
   const placement = (grid, marker) => eval(grid).assignMarker(marker);
 
-  const spaceList = () => spaces.forEach(space => console.log(space));
-
-  const spaceListNaked = () => spaces.forEach(space => console.log(String(space.grid)));
+  function spaceList() {
+    spacesArray = []
+    spaces.forEach(space => spacesArray.push(space));
+    console.log(spacesArray)
+    return spacesArray
+  }
 
   const boardUpdate = () => spaces.forEach(space => document.getElementById(space.grid).innerHTML = space.marker);
 
+  function nextTurn() {
+    step++;
+  }
+  
+  function checkWin() {
+    const letters = ["X", "O"];
+    var outcome = false;
+    letters.forEach(letter => {
+      if ((a1.marker === letter && a2.marker === letter && a3.marker === letter)  || 
+      (b1.marker === letter && b2.marker === letter && b3.marker === letter) ||
+      (c1.marker === letter && c2.marker === letter && c3.marker === letter) ||
+      (a1.marker === letter && b1.marker === letter && c1.marker === letter) ||
+      (a2.marker === letter && b2.marker === letter && c2.marker === letter) ||
+      (a3.marker === letter && b3.marker === letter && c3.marker === letter) ||
+      (a1.marker === letter && b2.marker === letter && c3.marker === letter) ||
+      (a3.marker === letter && b2.marker === letter && c1.marker === letter))
+      {
+        document.getElementById("message-board-message").innerHTML = `${currentTurn} wins!!!`
+        outcome = true
+      }
+    });
+    return outcome;
+  }
+
+  function whoseTurn() {
+    if (step % 2 == 0) {
+      currentTurn = "X";
+    } else {
+      currentTurn = "O";
+    }
+    document.getElementById("message-board-message").innerHTML = `Your Turn, ${currentTurn}`
+    return currentTurn;
+  }
   return {
     placement,
     spaceList,
-    spaceListNaked,
-    boardUpdate
+    boardUpdate,
+    nextTurn,
+    whoseTurn,
+    checkWin
   };
   
 
 })()
 
-const displayController = (() => {
-  
-  // gameBoard.spaceList().forEach(element => document.getElementById(element).addEventListener('click', console.log('click')));
+const flowController = (function() {
+
+})
+
+const displayController = (function() {
+
+  function placeMarker(grid) {
+    document.getElementById(grid).removeEventListener('click', function() {placeMarker(element.grid)});
+    gameBoard.placement(grid, gameBoard.whoseTurn());
+    gameBoard.boardUpdate();
+    if (gameBoard.checkWin() == false) {
+      gameBoard.nextTurn();
+      console.log(gameBoard.whoseTurn())}
+  }
+
+  function addListener(element) {
+    if (element.marker == "") {
+      document.getElementById(element.grid).addEventListener('click', function() { if (element.marker == "") {placeMarker(element.grid)}});
+    }
+  }
+
   gameBoard.boardUpdate()
-  gameBoard.spaceListNaked()
+  gameBoard.whoseTurn()
+  gameBoard.spaceList().forEach(element => addListener(element)) 
+  
 })()
+
+// set current turn
+// update message board with current turn
+// event listener for avail squares with current turn arg
+// 
